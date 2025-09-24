5916d3ea01fc94e67aed5aeacafca6ddd97218f8
Danylo <25032002ur@gmail.com>
2025-09-24 16:00:23 +0300

    added

--- apps/frontend/src/enviroments/enviroments.ts
+++ apps/frontend/src/enviroments/enviroments.ts
@@ -1,4 +1,16 @@
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:3000/api',
+// };
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api',
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  firebaseConfig: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'test-wf-updating.firebaseapp.com',
+    projectId: 'test-wf-updating',
+    storageBucket: 'test-wf-updating.firebasestorage.app',
+    messagingSenderId: '1091082410947',
+    appId: '1:1091082410947:web:647e55d04a231ae9b93b76',
+  },
 };
